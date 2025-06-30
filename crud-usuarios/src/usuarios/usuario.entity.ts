33b24da2ed51028ae2d2d140fa9a9f4e4d59e270
Misael <leasim3x@gmail.com>
2025-06-30 01:05:39 -0600

    Creando la entidad

---
+++ crud-usuarios/src/usuarios/usuario.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Usuario {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    email: string;
+
+    @Column({ default: true })
+    activo: boolean;
+}
