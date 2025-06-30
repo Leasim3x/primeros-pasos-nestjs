2fc013c5f2c48ff04efc062c9dd76a83f9c17a5a
Misael <leasim3x@gmail.com>
2025-06-30 00:49:19 -0600

    Configurando el TypeORM con PostgreSQL

--- crud-usuarios/src/app.module.ts
+++ crud-usuarios/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuarios/usuario.entity';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // Configuración del TypeORM con el PostgreSQL
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'invitado',
+      password: 'pass',
+      database: 'crud_nest_usuarios',
+      entities: [Usuario],
+      synchronize: true,  // Solo para el desarrollo
+    }),
+    UsuariosModule,
+  ],
 })
 export class AppModule {}
