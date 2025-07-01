0a2500c35e00b910486b59ef15e7e95b81345741
Misael <leasim3x@gmail.com>
2025-07-01 09:03:06 -0600

    Configuración del Controlador

--- crud-usuarios/src/usuarios/usuarios.controller.ts
+++ crud-usuarios/src/usuarios/usuarios.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
+import { Usuario } from './usuario.entity';
 
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+    constructor(private readonly usuariosService: UsuariosService) {}
+
+    @Post()
+    create(@Body() data: Partial<Usuario>) {
+        return this.usuariosService.create(data);
+    }
+
+    @Get()
+    findAll(){
+        return this.usuariosService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usuariosService.findOne(+id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() data: Partial<Usuario>) {
+        return this.usuariosService.update(+id, data)
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.usuariosService.remove(+id);
+    }
+}
