c66cb86b5f21329687d44989d5b0a4250ea7d003
Misael <leasim3x@gmail.com>
2025-07-01 08:51:39 -0600

    Creando la lógica en usuarios.service

--- crud-usuarios/src/usuarios/usuarios.service.ts
+++ crud-usuarios/src/usuarios/usuarios.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from './usuario.entity';
 
 @Injectable()
-export class UsuariosService {}
+export class UsuariosService {
+    constructor(
+        @InjectRepository(Usuario)
+        private usuarioRepo: Repository<Usuario>,
+    ){}
+
+    create(data: Partial<Usuario>){
+        const usuario = this.usuarioRepo.create(data);
+        return this.usuarioRepo.save(usuario);
+    }
+
+    findAll(){
+        return this.usuarioRepo.find();
+    }
+
+    findOne(id: number) {
+        return this.usuarioRepo.findOneBy({ id });
+    }
+
+    async update(id: number, data: Partial<Usuario>) {
+        await this.usuarioRepo.update(id, data);
+        return this.findOne(id);
+    }
+
+    async remove(id: number) {
+        const result = await this.usuarioRepo.delete(id);
+        return result.affected ? {eliminado: true} : { eliminado: false};
+    }
+}
